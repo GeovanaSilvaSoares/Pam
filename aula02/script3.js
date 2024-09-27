bdb8b532ec987efff46520eb6efcd50aa899b73f
GeovanaSilvaSoares <130610029+GeovanaSilvaSoares@users.noreply.github.com>
2024-09-27 07:45:30 -0300

    Add files via upload

---
+++ aula02/script3.js
@@ -0,0 +1,35 @@
+var valor1 = 9;
+var valor2 = 3;
+
+
+// Adição
+function adicao() {
+    var soma = valor1 + valor2;
+    console.log(valor1 + ' + ' + valor2 + ' = ' + soma);
+}
+
+adicao();
+
+// Subtração
+function subtracao() {
+    var soma = valor1 - valor2;
+    console.log(valor1 + ' - ' + valor2 + ' = ' + soma);
+}
+
+subtracao();
+
+// Divisão
+function divisao() {
+    var soma = valor1 / valor2;
+    console.log(valor1 + ' : ' + valor2 + ' = ' + soma);
+}
+
+divisao();
+
+// Multiplicação
+function multiplicacao() {
+    var soma = valor1 * valor2;
+    console.log(valor1 + ' * ' + valor2 + ' = ' + soma);
+}
+
+multiplicacao();
